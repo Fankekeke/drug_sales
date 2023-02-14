ecfb19c9a0159aee735a09745bad1ba807289cc3
Fankekeke <1258846421@qq.com>
2023-02-14 23:32:09 +0800

    fix -统计数据查询

--- frontend/src/views/statistics/sale/Sale.vue
+++ frontend/src/views/statistics/sale/Sale.vue
@@ -1,22 +1,34 @@
 <template>
   <a-card :bordered="false" class="card-area">
-    <div style="background:#ECECEC; padding:30px;margin-top: 30px">
+    <div style="background:#ECECEC; padding:30px;margin-top: 30px;margin-bottom: 30px">
       <a-row :gutter="30">
         <a-col :span="6" v-for="(item, index) in workStatusList" :key="index">
           <a-card :bordered="false">
             <span slot="title">
-              <a-badge status="processing" v-if="item.repair !== undefined"/>
-              <a-badge status="success" v-else/>
+              <a-badge status="processing"/>
               <span style="font-size: 14px;font-family: SimHei">
-                {{ item.staff.name }}
-                <span v-if="item.repair !== undefined">【工作】</span>
-                <span v-else>【空闲】</span>
+                {{ item.pharmacyName }}
+                <span style="margin-left: 15px">评价得分：【{{ item.score }}分】</span>
               </span>
             </span>
           </a-card>
         </a-col>
       </a-row>
     </div>
+    <a-row :gutter="20">
+      <a-col :span="12">
+        <a-card hoverable :bordered="false" style="width: 100%">
+          <a-skeleton active v-if="loading" />
+          <apexchart v-if="!loading" type="bar" height="300" :options="chartOptions1" :series="series1"></apexchart>
+        </a-card>
+      </a-col>
+      <a-col :span="12">
+        <a-card hoverable :bordered="false" style="width: 100%">
+          <a-skeleton active v-if="loading" />
+          <apexchart v-if="!loading" type="bar" height="300" :options="chartOptions2" :series="series2"></apexchart>
+        </a-card>
+      </a-col>
+    </a-row>
   </a-card>
 </template>
 
@@ -25,17 +37,109 @@ export default {
   name: 'Work',
   data () {
     return {
-      workStatusList: []
+      workStatusList: [],
+      loading: false,
+      series1: [],
+      chartOptions1: {
+        chart: {
+          type: 'bar',
+          height: 300
+        },
+        title: {
+          text: '十天内各家订单数量统计',
+          align: 'left'
+        },
+        plotOptions: {
+          bar: {
+            horizontal: false,
+            columnWidth: '55%'
+          }
+        },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          show: true,
+          width: 2,
+          colors: ['transparent']
+        },
+        xaxis: {
+          categories: []
+        },
+        yaxis: {
+          title: {
+            text: ''
+          }
+        },
+        fill: {
+          opacity: 1
+        },
+        tooltip: {
+          y: {
+            formatter: function (val) {
+              return val + ' 单'
+            }
+          }
+        }
+      },
+      series2: [],
+      chartOptions2: {
+        chart: {
+          type: 'bar',
+          height: 300
+        },
+        title: {
+          text: '十天内各家订单收益统计',
+          align: 'left'
+        },
+        plotOptions: {
+          bar: {
+            horizontal: false,
+            columnWidth: '55%'
+          }
+        },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          show: true,
+          width: 2,
+          colors: ['transparent']
+        },
+        xaxis: {
+          categories: []
+        },
+        yaxis: {
+          title: {
+            text: ''
+          }
+        },
+        fill: {
+          opacity: 1
+        },
+        tooltip: {
+          y: {
+            formatter: function (val) {
+              return val + ' 单'
+            }
+          }
+        }
+      }
     }
   },
   mounted () {
     this.getWorkStatusList()
+    this.selectOrderDays()
   },
   methods: {
     getWorkStatusList () {
-      this.$get(`/cos/staff-info/workStatus`).then((r) => {
+      this.$get(`/cos/pharmacy-info/evaluate/rank`).then((r) => {
         this.workStatusList = r.data.data
-        console.log(JSON.stringify(this.workStatusList))
+      })
+    },
+    selectOrderDays () {
+      this.$get(`/cos/pharmacy-info/selectOrderDays`).then((r) => {
+        console.log(JSON.stringify(r.data))
       })
     }
   }
