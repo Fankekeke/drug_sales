5e102528ab94ec33bbaf187896a4fa7fceed93c8
fanke <1258846421@qq.com>
2023-04-24 18:01:53 +0800

    fix -添加门户操作

---
+++ frontend/src/views/user/order/OrderView.vue
@@ -0,0 +1,207 @@
+<template>
+  <a-modal v-model="show" title="订单详情" @cancel="onClose" :width="900">
+    <template slot="footer">
+      <a-button key="back" @click="onClose" type="danger">
+        关闭
+      </a-button>
+    </template>
+    <div style="font-size: 13px;font-family: SimHei" v-if="orderData !== null">
+      <div style="padding-left: 24px;padding-right: 24px;margin-bottom: 50px;margin-top: 50px">
+        <a-steps :current="current" progress-dot size="small">
+          <a-step title="待付款" />
+          <a-step title="已下单" />
+          <a-step title="配送中" />
+          <a-step title="已收货" />
+        </a-steps>
+      </div>
+      <a-row style="padding-left: 24px;padding-right: 24px;">
+        <a-col style="margin-bottom: 15px"><span style="font-size: 15px;font-weight: 650;color: #000c17">基础信息</span></a-col>
+        <a-col :span="8"><b>工单编号：</b>
+          {{ orderData.code }}
+        </a-col>
+        <a-col :span="8"><b>客户名称：</b>
+          {{ orderData.userName ? orderData.userName : '- -' }}
+        </a-col>
+        <a-col :span="8"><b>联系方式：</b>
+          {{ orderData.phone ? orderData.phone : '- -' }}
+        </a-col>
+      </a-row>
+      <br/>
+      <a-row style="padding-left: 24px;padding-right: 24px;">
+        <a-col :span="8"><b>当前状态：</b>
+          <span v-if="orderData.orderStatus == 0">待付款</span>
+          <span v-if="orderData.orderStatus == 1">已下单</span>
+          <span v-if="orderData.orderStatus == 2">配送中</span>
+          <span v-if="orderData.orderStatus == 3">已收货</span>
+        </a-col>
+        <a-col :span="8"><b>订单金额：</b>
+          {{ orderData.totalCost }} 元
+        </a-col>
+        <a-col :span="8"><b>下单时间：</b>
+          {{ orderData.createDate }}
+        </a-col>
+      </a-row>
+      <br/>
+      <a-row style="padding-left: 24px;padding-right: 24px;">
+        <a-col style="margin-bottom: 15px"><span style="font-size: 15px;font-weight: 650;color: #000c17">药店信息</span></a-col>
+        <a-col :span="8"><b>药店名称：</b>
+            {{ orderData.pharmacyName }}
+          </a-col>
+        <a-col :span="8"><b>药店地址：</b>
+          {{ orderData.address }}
+        </a-col>
+        <a-col :span="8"><b>联系方式：</b>
+          {{ orderData.pharmacyPhone }}
+        </a-col>
+      </a-row>
+      <br/>
+      <a-row style="padding-left: 24px;padding-right: 24px;">
+        <a-col style="margin-bottom: 15px"><span style="font-size: 15px;font-weight: 650;color: #000c17">购买药品信息</span></a-col>
+         <a-col :span="24">
+          <a-table :columns="columns" :data-source="durgList">
+          </a-table>
+        </a-col>
+      </a-row>
+      <br/>
+      <a-row style="padding-left: 24px;padding-right: 24px;">
+        <a-col style="margin-bottom: 15px"><span style="font-size: 15px;font-weight: 650;color: #000c17">当前物流</span></a-col>
+         <a-col :span="24">
+          <a-table :columns="logisticsColumns" :data-source="logisticsList">
+          </a-table>
+        </a-col>
+      </a-row>
+    </div>
+  </a-modal>
+</template>
+
+<script>
+import moment from 'moment'
+moment.locale('zh-cn')
+function getBase64 (file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onload = () => resolve(reader.result)
+    reader.onerror = error => reject(error)
+  })
+}
+export default {
+  name: 'orderView',
+  props: {
+    orderShow: {
+      type: Boolean,
+      default: false
+    },
+    orderData: {
+      type: Object
+    }
+  },
+  computed: {
+    show: {
+      get: function () {
+        return this.orderShow
+      },
+      set: function () {
+      }
+    },
+    columns () {
+      return [{
+        title: '药品名称',
+        dataIndex: 'drugName'
+      }, {
+        title: '品牌',
+        dataIndex: 'brand'
+      }, {
+        title: '数量',
+        dataIndex: 'quantity'
+      }, {
+        title: '药品图片',
+        dataIndex: 'images',
+        customRender: (text, record, index) => {
+          if (!record.images) return <a-avatar shape="square" icon="user" />
+          return <a-popover>
+            <template slot="content">
+              <a-avatar shape="square" size={132} icon="user" src={ 'http://127.0.0.1:9527/imagesWeb/' + record.images.split(',')[0] } />
+            </template>
+            <a-avatar shape="square" icon="user" src={ 'http://127.0.0.1:9527/imagesWeb/' + record.images.split(',')[0] } />
+          </a-popover>
+        }
+      }, {
+        title: '单价',
+        dataIndex: 'unitPrice'
+      }]
+    },
+    logisticsColumns () {
+      return [{
+        title: '物流信息',
+        dataIndex: 'remark'
+      }, {
+        title: '操作时间',
+        dataIndex: 'createDate'
+      }]
+    }
+  },
+  data () {
+    return {
+      loading: false,
+      fileList: [],
+      previewVisible: false,
+      previewImage: '',
+      repairInfo: null,
+      reserveInfo: null,
+      durgList: [],
+      logisticsList: [],
+      current: 0
+    }
+  },
+  watch: {
+    orderShow: function (value) {
+      if (value) {
+        this.dataInit(this.orderData.id)
+        this.current = this.orderData.orderStatus
+      }
+    }
+  },
+  methods: {
+    dataInit (orderId) {
+      // 药品信息
+      this.$get(`/cos/order-detail/detail/${orderId}`).then((r) => {
+        this.durgList = r.data.data
+      })
+      // 物流信息
+      this.$get(`/cos/logistics-info/order/${orderId}`).then((r) => {
+        this.logisticsList = r.data.data
+      })
+    },
+    imagesInit (images) {
+      if (images !== null && images !== '') {
+        let imageList = []
+        images.split(',').forEach((image, index) => {
+          imageList.push({uid: index, name: image, status: 'done', url: 'http://127.0.0.1:9527/imagesWeb/' + image})
+        })
+        this.fileList = imageList
+      }
+    },
+    handleCancel () {
+      this.previewVisible = false
+    },
+    async handlePreview (file) {
+      if (!file.url && !file.preview) {
+        file.preview = await getBase64(file.originFileObj)
+      }
+      this.previewImage = file.url || file.preview
+      this.previewVisible = true
+    },
+    picHandleChange ({ fileList }) {
+      this.fileList = fileList
+    },
+    onClose () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/user/personal/Personal.vue
@@ -0,0 +1,188 @@
+<template>
+  <a-row :gutter="8" style="width: 100%">
+    <a-col :span="6">
+      <div style="background:#ECECEC; padding:30px;margin-top: 30px">
+        <a-card :bordered="false">
+          <b style="font-size: 15px">我的信息</b>
+        </a-card>
+        <a-card :bordered="false">
+          <a-form :form="form" layout="vertical">
+            <a-row :gutter="20">
+              <a-col :span="24">
+                <a-form-item label='客户姓名' v-bind="formItemLayout">
+                  <a-input disabled v-decorator="[
+                  'name',
+                  { rules: [{ required: true, message: '请输入客户姓名!' }] }
+                  ]"/>
+                </a-form-item>
+              </a-col>
+              <a-col :span="24">
+                <a-form-item label='联系电话' v-bind="formItemLayout">
+                  <a-input v-decorator="[
+                  'phone',
+                  { rules: [{ required: true, message: '请输入联系电话!' }] }
+                  ]"/>
+                </a-form-item>
+              </a-col>
+              <a-col :span="24">
+                <a-form-item label='收获地址' v-bind="formItemLayout">
+                  <a-input v-decorator="[
+                  'address',
+                  { rules: [{ required: true, message: '请输入收获地址!' }] }
+                  ]"/>
+                </a-form-item>
+              </a-col>
+              <a-col :span="24">
+                <a-form-item label='城市' v-bind="formItemLayout">
+                  <a-input v-decorator="[
+                  'city',
+                  { rules: [{ required: true, message: '请输入城市!' }] }
+                  ]"/>
+                </a-form-item>
+              </a-col>
+              <a-col :span="24">
+                <a-form-item label='区域' v-bind="formItemLayout">
+                  <a-input v-decorator="[
+                  'area',
+                  { rules: [{ required: true, message: '请输入区域!' }] }
+                  ]"/>
+                </a-form-item>
+              </a-col>
+              <a-col :span="24">
+                <a-form-item label='省份' v-bind="formItemLayout">
+                  <a-input v-decorator="[
+                  'province',
+                  { rules: [{ required: true, message: '请输入省份!' }] }
+                  ]"/>
+                </a-form-item>
+              </a-col>
+              <a-col :span="24">
+                <a-form-item>
+                  <a-button
+                    size="large"
+                    type="primary"
+                    htmlType="submit"
+                    class="register-button"
+                    style="width: 35%;float: right;margin-top: 50px"
+                    :loading="loading"
+                    @click.stop.prevent="handleSubmit"
+                    :disabled="loading">立即修改
+                  </a-button>
+                </a-form-item>
+              </a-col>
+            </a-row>
+          </a-form>
+        </a-card>
+      </div>
+    </a-col>
+    <a-col :span="18">
+      <div style="background:#ECECEC; padding:30px;margin-top: 30px">
+        <a-card :bordered="false">
+          <a-spin :spinning="dataLoading">
+            <a-calendar>
+              <ul slot="dateCellRender" slot-scope="value" class="events">
+                <li v-for="item in getListData(value)" :key="item.content">
+                  <a-badge :status="item.type" :text="item.content" />
+                </li>
+              </ul>
+            </a-calendar>
+          </a-spin>
+        </a-card>
+      </div>
+    </a-col>
+  </a-row>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+import moment from 'moment'
+moment.locale('zh-cn')
+const formItemLayout = {
+  labelCol: { span: 24 },
+  wrapperCol: { span: 24 }
+}
+export default {
+  name: 'User',
+  computed: {
+    ...mapState({
+      currentUser: state => state.account.user
+    })
+  },
+  data () {
+    return {
+      rowId: null,
+      formItemLayout,
+      form: this.$form.createForm(this),
+      loading: false,
+      courseInfo: [],
+      dataLoading: false
+    }
+  },
+  mounted () {
+    this.dataInit()
+  },
+  methods: {
+    isDuringDate (beginDateStr, endDateStr, curDataStr) {
+      let curDate = new Date(curDataStr)
+      let beginDate = new Date(beginDateStr)
+      let endDate = new Date(endDateStr)
+      if (curDate >= beginDate && curDate <= endDate) {
+        return true
+      }
+      return false
+    },
+    getListData (value) {
+      let listData = []
+      this.courseInfo.forEach(item => {
+        if ((moment(value).format('YYYY-MM-DD')) === (moment(item.createDate).format('YYYY-MM-DD'))) {
+          listData.push({type: 'success', content: item.remark})
+        }
+      })
+      return listData || []
+    },
+    dataInit () {
+      this.dataLoading = true
+      this.$get(`/cos/user-info/detail/${this.currentUser.userId}`).then((r) => {
+        this.rowId = r.data.user.id
+        this.setFormValues(r.data.user)
+        this.courseInfo = r.data.order
+        this.dataLoading = false
+      })
+    },
+    setFormValues ({...student}) {
+      this.rowId = student.id
+      let fields = ['name', 'phone', 'address', 'province', 'city', 'area']
+      let obj = {}
+      Object.keys(student).forEach((key) => {
+        if (fields.indexOf(key) !== -1) {
+          this.form.getFieldDecorator(key)
+          obj[key] = student[key]
+        }
+        if (key === 'sex' && student[key] !== null) {
+          obj[key] = student[key].toString()
+        }
+      })
+      this.form.setFieldsValue(obj)
+    },
+    handleSubmit () {
+      this.form.validateFields((err, values) => {
+        values.id = this.rowId
+        if (!err) {
+          this.loading = true
+          this.$put('/cos/user-info', {
+            ...values
+          }).then((r) => {
+            this.$message.success('修改信息成功')
+            this.loading = false
+          }).catch(() => {
+            this.loading = false
+          })
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
