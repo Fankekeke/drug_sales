53b92ccefaed9c3ee976b2d1460e5d30b10dae78
fanke <1258846421@qq.com>
2023-04-25 09:24:41 +0800

    fix -添加门户操作

---
+++ frontend/src/views/user/evaluate/Evaluate.vue
@@ -0,0 +1,307 @@
+<template>
+  <a-card :bordered="false" class="card-area">
+    <div :class="advanced ? 'search' : null">
+      <!-- 搜索区域 -->
+      <a-form layout="horizontal">
+        <a-row :gutter="15">
+          <div :class="advanced ? null: 'fold'">
+            <a-col :md="6" :sm="24">
+              <a-form-item
+                label="订单编号"
+                :labelCol="{span: 5}"
+                :wrapperCol="{span: 18, offset: 1}">
+                <a-input v-model="queryParams.orderCode"/>
+              </a-form-item>
+            </a-col>
+            <a-col :md="6" :sm="24">
+              <a-form-item
+                label="客户名称"
+                :labelCol="{span: 5}"
+                :wrapperCol="{span: 18, offset: 1}">
+                <a-input v-model="queryParams.userName"/>
+              </a-form-item>
+            </a-col>
+            <a-col :md="6" :sm="24">
+              <a-form-item
+                label="药店名称"
+                :labelCol="{span: 5}"
+                :wrapperCol="{span: 18, offset: 1}">
+                <a-input v-model="queryParams.pharmacyName"/>
+              </a-form-item>
+            </a-col>
+          </div>
+          <span style="float: right; margin-top: 3px;">
+            <a-button type="primary" @click="search">查询</a-button>
+            <a-button style="margin-left: 8px" @click="reset">重置</a-button>
+          </span>
+        </a-row>
+      </a-form>
+    </div>
+    <div>
+      <div class="operator">
+        <a-button @click="batchDelete">删除</a-button>
+      </div>
+      <!-- 表格区域 -->
+      <a-table ref="TableInfo"
+               :columns="columns"
+               :rowKey="record => record.id"
+               :dataSource="dataSource"
+               :pagination="pagination"
+               :loading="loading"
+               :rowSelection="{selectedRowKeys: selectedRowKeys, onChange: onSelectChange}"
+               :scroll="{ x: 900 }"
+               @change="handleTableChange">
+        <template slot="contentShow" slot-scope="text, record">
+          <template>
+            <a-tooltip>
+              <template slot="title">
+                {{ record.content }}
+              </template>
+              {{ record.content.slice(0, 10) }} ...
+            </a-tooltip>
+          </template>
+        </template>
+      </a-table>
+    </div>
+  </a-card>
+</template>
+
+<script>
+import RangeDate from '@/components/datetime/RangeDate'
+import {mapState} from 'vuex'
+import moment from 'moment'
+moment.locale('zh-cn')
+
+export default {
+  name: 'evaluate',
+  components: {RangeDate},
+  data () {
+    return {
+      advanced: false,
+      evaluateAdd: {
+        visiable: false
+      },
+      evaluateEdit: {
+        visiable: false
+      },
+      queryParams: {},
+      filteredInfo: null,
+      sortedInfo: null,
+      paginationInfo: null,
+      dataSource: [],
+      selectedRowKeys: [],
+      loading: false,
+      pagination: {
+        pageSizeOptions: ['10', '20', '30', '40', '100'],
+        defaultCurrent: 1,
+        defaultPageSize: 10,
+        showQuickJumper: true,
+        showSizeChanger: true,
+        showTotal: (total, range) => `显示 ${range[0]} ~ ${range[1]} 条记录，共 ${total} 条记录`
+      },
+      userList: []
+    }
+  },
+  computed: {
+    ...mapState({
+      currentUser: state => state.account.user
+    }),
+    columns () {
+      return [{
+        title: '订单编号',
+        dataIndex: 'code'
+      }, {
+        title: '订单总价',
+        dataIndex: 'totalCost',
+        customRender: (text, row, index) => {
+          if (text !== null) {
+            return text + '元'
+          } else {
+            return '- -'
+          }
+        }
+      }, {
+        title: '评价客户',
+        dataIndex: 'name'
+      }, {
+        title: '药房名称',
+        dataIndex: 'pharmacyName'
+      }, {
+        title: '评价得分',
+        dataIndex: 'score',
+        customRender: (text, row, index) => {
+          if (text !== null) {
+            return text + '分'
+          } else {
+            return '- -'
+          }
+        }
+      }, {
+        title: '评价内容',
+        dataIndex: 'content',
+        scopedSlots: { customRender: 'contentShow' }
+      }, {
+        title: '评价图片',
+        dataIndex: 'images',
+        customRender: (text, record, index) => {
+          if (!record.images) return <a-avatar shape="square" icon="user" />
+          return <a-popover>
+            <template slot="content">
+              <a-avatar shape="square" size={132} icon="user" src={ 'http://127.0.0.1:9527/imagesWeb/' + record.images.split(',')[0] } />
+            </template>
+            <a-avatar shape="square" icon="user" src={ 'http://127.0.0.1:9527/imagesWeb/' + record.images.split(',')[0] } />
+          </a-popover>
+        }
+      }, {
+        title: '评价时间',
+        dataIndex: 'createDate',
+        customRender: (text, row, index) => {
+          if (text !== null) {
+            return text
+          } else {
+            return '- -'
+          }
+        }
+      }]
+    }
+  },
+  mounted () {
+    this.fetch()
+  },
+  methods: {
+    onSelectChange (selectedRowKeys) {
+      this.selectedRowKeys = selectedRowKeys
+    },
+    toggleAdvanced () {
+      this.advanced = !this.advanced
+    },
+    add () {
+      this.evaluateAdd.visiable = true
+    },
+    handleevaluateAddClose () {
+      this.evaluateAdd.visiable = false
+    },
+    handleevaluateAddSuccess () {
+      this.evaluateAdd.visiable = false
+      this.$message.success('新增评价成功')
+      this.search()
+    },
+    edit (record) {
+      this.$refs.evaluateEdit.setFormValues(record)
+      this.evaluateEdit.visiable = true
+    },
+    handleevaluateEditClose () {
+      this.evaluateEdit.visiable = false
+    },
+    handleevaluateEditSuccess () {
+      this.evaluateEdit.visiable = false
+      this.$message.success('修改评价成功')
+      this.search()
+    },
+    handleDeptChange (value) {
+      this.queryParams.deptId = value || ''
+    },
+    batchDelete () {
+      if (!this.selectedRowKeys.length) {
+        this.$message.warning('请选择需要删除的记录')
+        return
+      }
+      let that = this
+      this.$confirm({
+        title: '确定删除所选中的记录?',
+        content: '当您点击确定按钮后，这些记录将会被彻底删除',
+        centered: true,
+        onOk () {
+          let ids = that.selectedRowKeys.join(',')
+          that.$delete('/cos/order-evaluate/' + ids).then(() => {
+            that.$message.success('删除成功')
+            that.selectedRowKeys = []
+            that.search()
+          })
+        },
+        onCancel () {
+          that.selectedRowKeys = []
+        }
+      })
+    },
+    search () {
+      let {sortedInfo, filteredInfo} = this
+      let sortField, sortOrder
+      // 获取当前列的排序和列的过滤规则
+      if (sortedInfo) {
+        sortField = sortedInfo.field
+        sortOrder = sortedInfo.order
+      }
+      this.fetch({
+        sortField: sortField,
+        sortOrder: sortOrder,
+        ...this.queryParams,
+        ...filteredInfo
+      })
+    },
+    reset () {
+      // 取消选中
+      this.selectedRowKeys = []
+      // 重置分页
+      this.$refs.TableInfo.pagination.current = this.pagination.defaultCurrent
+      if (this.paginationInfo) {
+        this.paginationInfo.current = this.pagination.defaultCurrent
+        this.paginationInfo.pageSize = this.pagination.defaultPageSize
+      }
+      // 重置列过滤器规则
+      this.filteredInfo = null
+      // 重置列排序规则
+      this.sortedInfo = null
+      // 重置查询参数
+      this.queryParams = {}
+      this.fetch()
+    },
+    handleTableChange (pagination, filters, sorter) {
+      // 将这三个参数赋值给Vue data，用于后续使用
+      this.paginationInfo = pagination
+      this.filteredInfo = filters
+      this.sortedInfo = sorter
+
+      this.fetch({
+        sortField: sorter.field,
+        sortOrder: sorter.order,
+        ...this.queryParams,
+        ...filters
+      })
+    },
+    fetch (params = {}) {
+      // 显示loading
+      this.loading = true
+      if (this.paginationInfo) {
+        // 如果分页信息不为空，则设置表格当前第几页，每页条数，并设置查询分页参数
+        this.$refs.TableInfo.pagination.current = this.paginationInfo.current
+        this.$refs.TableInfo.pagination.pageSize = this.paginationInfo.pageSize
+        params.size = this.paginationInfo.pageSize
+        params.current = this.paginationInfo.current
+      } else {
+        // 如果分页信息为空，则设置为默认值
+        params.size = this.pagination.defaultPageSize
+        params.current = this.pagination.defaultCurrent
+      }
+      if (params.type === undefined) {
+        delete params.type
+      }
+      this.$get('/cos/order-evaluate/page', {
+        ...params
+      }).then((r) => {
+        let data = r.data.data
+        const pagination = {...this.pagination}
+        pagination.total = data.total
+        this.dataSource = data.records
+        this.pagination = pagination
+        // 数据加载完毕，关闭loading
+        this.loading = false
+      })
+    }
+  },
+  watch: {}
+}
+</script>
+<style lang="less" scoped>
+@import "../../../../static/less/Common";
+</style>
---
+++ frontend/src/views/user/record/Record.vue
@@ -0,0 +1,319 @@
+<template>
+  <a-card :bordered="false" class="card-area">
+    <div :class="advanced ? 'search' : null">
+      <!-- 搜索区域 -->
+      <a-form layout="horizontal">
+        <a-row :gutter="15">
+          <div :class="advanced ? null: 'fold'">
+            <a-col :md="6" :sm="24">
+              <a-form-item
+                label="订单编号"
+                :labelCol="{span: 5}"
+                :wrapperCol="{span: 18, offset: 1}">
+                <a-input v-model="queryParams.orderCode"/>
+              </a-form-item>
+            </a-col>
+            <a-col :md="6" :sm="24">
+              <a-form-item
+                label="客户名称"
+                :labelCol="{span: 5}"
+                :wrapperCol="{span: 18, offset: 1}">
+                <a-input v-model="queryParams.userName"/>
+              </a-form-item>
+            </a-col>
+          </div>
+          <span style="float: right; margin-top: 3px;">
+            <a-button type="primary" @click="search">查询</a-button>
+            <a-button style="margin-left: 8px" @click="reset">重置</a-button>
+          </span>
+        </a-row>
+      </a-form>
+    </div>
+    <div>
+      <div class="operator">
+        <a-button @click="batchDelete">删除</a-button>
+      </div>
+      <!-- 表格区域 -->
+      <a-table ref="TableInfo"
+               :columns="columns"
+               :rowKey="record => record.id"
+               :dataSource="dataSource"
+               :pagination="pagination"
+               :loading="loading"
+               :rowSelection="{selectedRowKeys: selectedRowKeys, onChange: onSelectChange}"
+               :scroll="{ x: 900 }"
+               @change="handleTableChange">
+        <template slot="titleShow" slot-scope="text, record">
+          <template>
+            <a-tooltip>
+              <template slot="title">
+                {{ record.title }}
+              </template>
+              {{ record.title.slice(0, 8) }} ...
+            </a-tooltip>
+          </template>
+        </template>
+      </a-table>
+    </div>
+  </a-card>
+</template>
+
+<script>
+import RangeDate from '@/components/datetime/RangeDate'
+import {mapState} from 'vuex'
+import moment from 'moment'
+moment.locale('zh-cn')
+
+export default {
+  name: 'payment',
+  components: {RangeDate},
+  data () {
+    return {
+      advanced: false,
+      paymentAdd: {
+        visiable: false
+      },
+      paymentEdit: {
+        visiable: false
+      },
+      queryParams: {},
+      filteredInfo: null,
+      sortedInfo: null,
+      paginationInfo: null,
+      dataSource: [],
+      selectedRowKeys: [],
+      loading: false,
+      pagination: {
+        pageSizeOptions: ['10', '20', '30', '40', '100'],
+        defaultCurrent: 1,
+        defaultPageSize: 10,
+        showQuickJumper: true,
+        showSizeChanger: true,
+        showTotal: (total, range) => `显示 ${range[0]} ~ ${range[1]} 条记录，共 ${total} 条记录`
+      },
+      userList: []
+    }
+  },
+  computed: {
+    ...mapState({
+      currentUser: state => state.account.user
+    }),
+    columns () {
+      return [{
+        title: '订单编号',
+        dataIndex: 'orderCode'
+      }, {
+        title: '客户名称',
+        dataIndex: 'userName',
+        customRender: (text, row, index) => {
+          if (text !== null) {
+            return text
+          } else {
+            return <a-tag>平台内下单</a-tag>
+          }
+        }
+      }, {
+        title: '联系方式',
+        dataIndex: 'phone',
+        customRender: (text, row, index) => {
+          if (text !== null) {
+            return text
+          } else {
+            return '- -'
+          }
+        }
+      }, {
+        title: '缴费金额',
+        dataIndex: 'money',
+        customRender: (text, row, index) => {
+          if (text !== null) {
+            return text + '元'
+          } else {
+            return '- -'
+          }
+        }
+      }, {
+        title: '药店名称',
+        dataIndex: 'pharmacyName',
+        customRender: (text, row, index) => {
+          if (text !== null) {
+            return text
+          } else {
+            return '- -'
+          }
+        }
+      }, {
+        title: '药店图片',
+        dataIndex: 'images',
+        customRender: (text, record, index) => {
+          if (!record.images) return <a-avatar shape="square" icon="user" />
+          return <a-popover>
+            <template slot="content">
+              <a-avatar shape="square" size={132} icon="user" src={ 'http://127.0.0.1:9527/imagesWeb/' + record.images.split(',')[0] } />
+            </template>
+            <a-avatar shape="square" icon="user" src={ 'http://127.0.0.1:9527/imagesWeb/' + record.images.split(',')[0] } />
+          </a-popover>
+        }
+      }, {
+        title: '药店地址',
+        dataIndex: 'address',
+        customRender: (text, row, index) => {
+          if (text !== null) {
+            return text
+          } else {
+            return '- -'
+          }
+        }
+      }, {
+        title: '缴费时间',
+        dataIndex: 'createDate',
+        customRender: (text, row, index) => {
+          if (text !== null) {
+            return text
+          } else {
+            return '- -'
+          }
+        }
+      }]
+    }
+  },
+  mounted () {
+    this.fetch()
+  },
+  methods: {
+    onSelectChange (selectedRowKeys) {
+      this.selectedRowKeys = selectedRowKeys
+    },
+    toggleAdvanced () {
+      this.advanced = !this.advanced
+    },
+    add () {
+      this.paymentAdd.visiable = true
+    },
+    handlepaymentAddClose () {
+      this.paymentAdd.visiable = false
+    },
+    handlepaymentAddSuccess () {
+      this.paymentAdd.visiable = false
+      this.$message.success('新增产品成功')
+      this.search()
+    },
+    edit (record) {
+      this.$refs.paymentEdit.setFormValues(record)
+      this.paymentEdit.visiable = true
+    },
+    handlepaymentEditClose () {
+      this.paymentEdit.visiable = false
+    },
+    handlepaymentEditSuccess () {
+      this.paymentEdit.visiable = false
+      this.$message.success('修改产品成功')
+      this.search()
+    },
+    handleDeptChange (value) {
+      this.queryParams.deptId = value || ''
+    },
+    batchDelete () {
+      if (!this.selectedRowKeys.length) {
+        this.$message.warning('请选择需要删除的记录')
+        return
+      }
+      let that = this
+      this.$confirm({
+        title: '确定删除所选中的记录?',
+        content: '当您点击确定按钮后，这些记录将会被彻底删除',
+        centered: true,
+        onOk () {
+          let ids = that.selectedRowKeys.join(',')
+          that.$delete('/cos/payment-record/' + ids).then(() => {
+            that.$message.success('删除成功')
+            that.selectedRowKeys = []
+            that.search()
+          })
+        },
+        onCancel () {
+          that.selectedRowKeys = []
+        }
+      })
+    },
+    search () {
+      let {sortedInfo, filteredInfo} = this
+      let sortField, sortOrder
+      // 获取当前列的排序和列的过滤规则
+      if (sortedInfo) {
+        sortField = sortedInfo.field
+        sortOrder = sortedInfo.order
+      }
+      this.fetch({
+        sortField: sortField,
+        sortOrder: sortOrder,
+        ...this.queryParams,
+        ...filteredInfo
+      })
+    },
+    reset () {
+      // 取消选中
+      this.selectedRowKeys = []
+      // 重置分页
+      this.$refs.TableInfo.pagination.current = this.pagination.defaultCurrent
+      if (this.paginationInfo) {
+        this.paginationInfo.current = this.pagination.defaultCurrent
+        this.paginationInfo.pageSize = this.pagination.defaultPageSize
+      }
+      // 重置列过滤器规则
+      this.filteredInfo = null
+      // 重置列排序规则
+      this.sortedInfo = null
+      // 重置查询参数
+      this.queryParams = {}
+      this.fetch()
+    },
+    handleTableChange (pagination, filters, sorter) {
+      // 将这三个参数赋值给Vue data，用于后续使用
+      this.paginationInfo = pagination
+      this.filteredInfo = filters
+      this.sortedInfo = sorter
+
+      this.fetch({
+        sortField: sorter.field,
+        sortOrder: sorter.order,
+        ...this.queryParams,
+        ...filters
+      })
+    },
+    fetch (params = {}) {
+      // 显示loading
+      this.loading = true
+      if (this.paginationInfo) {
+        // 如果分页信息不为空，则设置表格当前第几页，每页条数，并设置查询分页参数
+        this.$refs.TableInfo.pagination.current = this.paginationInfo.current
+        this.$refs.TableInfo.pagination.pageSize = this.paginationInfo.pageSize
+        params.size = this.paginationInfo.pageSize
+        params.current = this.paginationInfo.current
+      } else {
+        // 如果分页信息为空，则设置为默认值
+        params.size = this.pagination.defaultPageSize
+        params.current = this.pagination.defaultCurrent
+      }
+      if (params.type === undefined) {
+        delete params.type
+      }
+      this.$get('/cos/payment-record/page', {
+        ...params
+      }).then((r) => {
+        let data = r.data.data
+        const pagination = {...this.pagination}
+        pagination.total = data.total
+        this.dataSource = data.records
+        this.pagination = pagination
+        // 数据加载完毕，关闭loading
+        this.loading = false
+      })
+    }
+  },
+  watch: {}
+}
+</script>
+<style lang="less" scoped>
+@import "../../../../static/less/Common";
+</style>
