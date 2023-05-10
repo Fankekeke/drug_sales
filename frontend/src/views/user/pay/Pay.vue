52a30cefa92ac837d463e66633654dd006a5c8d7
fanke <1258846421@qq.com>
2023-05-10 15:13:58 +0800

    fix -支付成功回调

---
+++ frontend/src/views/user/pay/Pay.vue
@@ -0,0 +1,34 @@
+<template>
+  <div style="width: 100%">
+    <p style="text-align: center;margin-top: 50px"><a-icon type="smile" theme="twoTone" style="font-size: 100px;" /></p>
+    <p style="font-size: 26px;text-align: center;margin-top: 5px">支付成功</p>
+    <p style="text-align: center">
+      <a-button type="primary" @click="$router.push('/user/payment')">
+        返回
+      </a-button>
+    </p>
+  </div>
+</template>
+
+<script>
+import { getUrlKey } from '@/utils/urlKey'
+import {mapState} from 'vuex'
+export default {
+  name: 'Pay',
+  computed: {
+    ...mapState({
+      currentUser: state => state.account.user
+    })
+  },
+  mounted () {
+    if (getUrlKey('out_trade_no') !== null) {
+      this.$post('/cos/order-info/payment', { orderCode: getUrlKey('out_trade_no') }).then((r) => {
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
